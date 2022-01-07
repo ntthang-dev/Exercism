e3150a107e3b6724e5acab7223817426670e6eeb
ntthang-dev <ntthang.dev@gmail.com>
2022-01-07 20:22:58 +0700

    resitor-color-solution

---
+++ c/resistor-color/resistor_color.c
@@ -0,0 +1,16 @@
+#include "resistor_color.h"
+
+int color_code(resistor_band_t color)
+{
+    return color;
+}
+resistor_band_t *colors()
+{
+    // static array elements inited to BLACK
+    static resistor_band_t bands[10];
+    if (bands[1] == BROWN)
+        return bands;
+    for (resistor_band_t band = BLACK; band <= WHITE; band++)
+        bands[band] = band;
+    return bands;
+}
