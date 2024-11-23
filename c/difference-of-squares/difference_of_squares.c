c9f1d735bca71da1f5da273509e91d246ec92b12
ntthang-dev <ntthang.dev@gmail.com>
2024-11-23 13:32:44 +0700

    Solution difference of square - C

--- c/difference-of-squares/difference_of_squares.c
+++ c/difference-of-squares/difference_of_squares.c
@@ -1,1 +1,19 @@
 #include "difference_of_squares.h"
+
+// Sum of squares function
+unsigned int sum_of_squares(unsigned int number)
+{
+    return (number) * (number+ 1) * (2*number + 1) / 6;  
+}
+
+// Square of sum  function
+unsigned int square_of_sum(unsigned int number)
+{
+    return (number*  (number + 1)) * (number*  (number + 1)) / 4;
+}
+
+unsigned int difference_of_squares(unsigned int number)
+{
+    return square_of_sum(number) - sum_of_squares(number);
+}
+
